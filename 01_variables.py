f96a586af13b04ffb8d805bd03cab2bbce81a172
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2022-08-19 19:29:53 +0400

    task 1

---
+++ 01_variables.py
@@ -0,0 +1,23 @@
+# integers and floats
+
+x = 15  # integer
+price = 9.99  # float
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+
+# strings
+name = 'Rolf'
+
+print(name)
+print(name * 2)
+
+# assignment
+a = 25
+b = a
+print(a, b, sep='\n')
+
+b = 17
+print(a, b, sep='\n')
